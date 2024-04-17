d011312db6e719b127daab6497e2b840138c33d8
Bart Stevens <bart.stevens9@gmail.com>
2024-04-17 18:40:59 +0200

    icons af

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -1,5 +1,5 @@
 export default function Icon ({shape, onClick, starred=false}) {
-const classes ="glyphicon glyphicon-"+shape + starred ? ' starred' : ''
+const classes ="glyphicon glyphicon-"+shape + (starred ? ' starred' : '')
 
     return (
     <i
