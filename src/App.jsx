b66c0583415ba1734c7ec789d0c7c08618f0ee9b
Amplifiction <bart.stevens9@gmail.com>
2024-04-20 17:00:54 +0200

    crud update complete + details

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,18 @@ import HeaderButton from './HeaderButton';
 import NoteEditor from './NoteEditor';
 
 function App() {
-//STATES
-const [activeNoteId, setActiveNoteId] = useState (-1)
-const [activeButton, setActiveButton] = useState ('all')
+    //STATES
+    const [activeNoteId, setActiveNoteId] = useState (-1)
+    const [activeButton, setActiveButton] = useState ('all')
 
-//CONSTANTS --> states van maken?
-const activeNote = notes.find(note => note.id === activeNoteId)
-const isStarred = activeNote? activeNote.favo : false
-const favoNotes = notes.filter(note => note.favo==true)
-
-
-//CRUD
+    //CRUD
     const [notes, dispatch] = useReducer(notesReducer, [{
         id:crypto.randomUUID(),
         content:
-            'Actieve branch: REDUCER.\n'+
-            'Git branch \'main\'=adhv reducer ;  branch \'noteContext\'=adhv context.',
+            'Actieve branch = REDUCER.\n'+
+            'Git branch \'main\'=adhv reducer ;  branch \'noteContext\'=adhv context.\n'+
+            '\n'+
+            'TO DO: index.css > .disabledIcon cursor not allowed werkt niet meer. (beide branches)',
         favo: true
     }])
     const handleAddNote = () => {
@@ -38,7 +34,7 @@ const favoNotes = notes.filter(note => note.favo==true)
         dispatch({type: 'editNote', id:activeNoteId, content})
     }
     const handleDelNote = () => {
-        if (activeNoteId==-1) {
+        if (activeNoteId===-1) {
             alert('Please select a note')
         } else {
             dispatch({type:'delNote', id:activeNoteId})
@@ -51,86 +47,80 @@ const favoNotes = notes.filter(note => note.favo==true)
         }
     }
     const handleFavNote = () => {
-        if (activeNoteId==-1) {
+        if (activeNoteId===-1) {
             alert('Please select a note')
         } else {
             dispatch({type:'favNote', id:activeNoteId})
-            //....
-        }
-
-        if (activeButton=='fav') {
-            setActiveNoteId(-1)
-        }
-        switchToAll()
-    }
-    const dryHandle = (type) => {
-        if (activeNoteId==-1) {
-            alert('Please select a note')
-        } else {
-            dispatch({type:type, id:activeNoteId})
-        }
-    }
-    const switchToAll = () => {
-        if (activeButton=='fav' && favoNotes.length==1) {
-            //==1 ipv <1 omdat dispatch (net als states) pas de volgende render leeg gemaakt wordt.
-            //Op het moment dat deze check gebeurt, is de lijst nog niet leeg.
-            setActiveButton('all')
+            if (activeButton=='fav') {
+                if (favoNotes.length===1) { //de favo lijst wordt leeg bij volgende render en dus switchen naar all notes, met behoud van activeNoteId
+                    setActiveButton('all')
+                } else { //de favo lijst wordt niet leeg en dus daar blijven, zodat gebruiker evt andere notes uit favo notes kan halen. Note deslecteren omdat die anders onder all notes geslecteerd staat, onzichtbaar voor gebruiker.
+                    setActiveNoteId(-1)
+                }
+            }
         }
     }//einde CRUD
 
-return (
-<div id="app">
+    //CONSTANTS
+    //--> states van maken?
+    //--> constants die gebruik maken van notes moeten staan ONDER de initialisatie van notes.
+    const activeNote = notes.find(note => note.id === activeNoteId)
+    const isStarred = activeNote? activeNote.favo : false
+    const favoNotes = notes.filter(note => note.favo==true)
 
-    <div id="toolbar">
-        <Icon
-            shape='plus'
-            onClick={handleAddNote}
-        />
-        <Icon
-            shape='star'
-            onClick={handleFavNote}
-            starred={isStarred}
-            disabled={!activeNote}
-        />
-        <Icon
-            shape='remove'
-            onClick={handleDelNote}
-            disabled={!activeNote}
-        />
-    </div>
+    return (
+        <div id="app">
 
-    <div id="notes-list">
-        <div id="list-header">
-            <h2>Notes</h2>
-            <div className="btn-group btn-group-justified">
-                <HeaderButton
-                    name='All notes'
-                    onClick={() => setActiveButton('all')}
-                    active={activeButton=='all'}
+            <div id="toolbar">
+                <Icon
+                    shape='plus'
+                    onClick={handleAddNote}
                 />
-                <HeaderButton
-                    name='Favorites'
-                    onClick={() => setActiveButton('fav')}
-                    active={activeButton=='fav'}
-                    disabled={favoNotes.length<1}
+                <Icon
+                    shape='star'
+                    onClick={handleFavNote}
+                    starred={isStarred}
+                    disabled={!activeNote}
+                />
+                <Icon
+                    shape='remove'
+                    onClick={handleDelNote}
+                    disabled={!activeNote}
                 />
             </div>
-        </div>
-        <NoteButtonList
-            array={activeButton=='all'? notes : favoNotes}
-            onClick={setActiveNoteId}
-            activeId={activeNoteId}
-            activeButton={activeButton}
-        />
-    </div>
 
-    <NoteEditor
-        note={activeNote? activeNote : 'noNote'}
-        onChange={handleEditNote}
-    />
+            <div id="notes-list">
+                <div id="list-header">
+                    <h2>Notes</h2>
+                    <div className="btn-group btn-group-justified">
+                        <HeaderButton
+                            name='All notes'
+                            onClick={() => setActiveButton('all')}
+                            active={activeButton=='all'}
+                        />
+                        <HeaderButton
+                            name='Favorites'
+                            onClick={() => setActiveButton('fav')}
+                            active={activeButton=='fav'}
+                            disabled={favoNotes.length<1}
+                        />
+                    </div>
+                </div>
+                <NoteButtonList
+                    array={activeButton=='all'? notes : favoNotes}
+                    onClick={setActiveNoteId}
+                    activeId={activeNoteId}
+                    activeButton={activeButton}
+                />
+            </div>
 
-</div> //einde app
-) //einde return
+            <NoteEditor
+                note={activeNote? activeNote : 'noNote'}
+                onChange={handleEditNote}
+            />
+
+        </div> //einde app
+    ) //einde return
 } //einde App()
 
 export default App;
